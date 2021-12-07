ecdf3bce0c2c65c236890ff0da15678edd7b853f
Cynthia Anderson <cind@ozguitarworks.com>
2021-12-07 13:26:48 -0800

    Initial Commit

---
+++ BetaOGW/BetaOGWApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaOGWApp.swift
+//  BetaOGW
+//
+//  Created by Cynthia Anderson on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct BetaOGWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
